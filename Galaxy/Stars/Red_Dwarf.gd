33a0df725750a093e9d43dcd31f13676f2853ae9
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:52:07 +0100

    Update Red_Dwarf.gd

--- Galaxy/Stars/Red_Dwarf.gd
+++ Galaxy/Stars/Red_Dwarf.gd
@@ -3,13 +3,19 @@ extends StaticBody2D
 var star_size = randf_range(0.05,0.45)
 var star_radius = 0.0
 
+@export var probability: float = 0.17
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	generate_RedDwarf()
 	
 func generate_RedDwarf():
-	$Red_Dwarf.scale = Vector2(star_size,star_size)
-	star_radius = star_size / 2
+	var ReddwarfSize = $Red_Dwarf.scale
+	var ReddwarfRad = $ReddwarfRadius.shape.radius
+	var ReddwarfRadSize = $ReddwarfRadius.scale
 	
-	self.position = Vector2(250,250)
+	ReddwarfSize = Vector2(star_size,star_size)
+	ReddwarfRadSize = ReddwarfSize * 2
+	
+	star_radius = ReddwarfRadSize.length() * ReddwarfRad
