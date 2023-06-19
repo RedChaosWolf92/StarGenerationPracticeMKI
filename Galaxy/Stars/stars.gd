b531e411a1e6d0b1778c4a30021eed5c8110dc02
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:53:37 +0100

    Update stars.gd

--- Galaxy/Stars/stars.gd
+++ Galaxy/Stars/stars.gd
@@ -8,6 +8,9 @@ const Spiral_tight = 55
 var numArms = 2
 var blackHole = preload("res://Galaxy/Stars/BlackHole.tscn").instantiate() # gets the black hole node in Stars
 
+var angle_increment = PI / 250 #controlls the tightness of the spiral, higher makes spiral looser and lower makes spiral tighter
+var current_angle = 0 # starting angle
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,11 +20,14 @@ func _ready():
 	for child in get_children():
 		if child is StaticBody2D:
 			star_types.append(child)
+			child.hide()
 			
 func centralBlackHole():
 	blackHole.global_position = Vector2(get_viewport_rect().size/2) # center location of the screen
-	blackHole.scale = Vector2(0.85,0.85)
+	blackHole.scale = Vector2(1.5,1.5)
+	blackHole.z_index = 1
 	add_child(blackHole)
+	blackHole.show()
 	print(blackHole.global_position, "this is where the blackhole is ")
 	print(blackHole.scale, "this is how large the black hole is")
 	print(" ")
@@ -44,43 +50,41 @@ func chooseStar():
 			return get_child(ct)
 			
 	return get_child(0)
-
-#func is_position_valid(newStar,pos):
-#	for star in get_children():
-#		var starDis = star.global_position.distance_to(pos)
-#		if starDis < newStar.star_radius + star.star_radius:
-#			return false
-#	return true
 			
 func add_star(starsNum: int):
 	var newStar = chooseStar().duplicate()
 	var Arms = starsNum % numArms
+	#finding the safe distance from blackhole
+	var safeDis = blackHole.star_radius + starsNum * 15
+	print(safeDis)
 	#position of stars
 	var pos
-	var attempts = 0
-	#finding the safe distance from blackhole
-	var safeDis = blackHole.star_radius
-	#if first star (starnums ==0), then generate it at least position equal to safe_distance
-	print(safeDis)
-	if starsNum == 0:
-		pos = get_viewport_rect().size / 2 + Vector2(0, safeDis)
-		print(pos, " safe distance from the blackhole")
-	else:
-		pos = get_viewport_rect().size / 2 + generate_spiral(starsNum, Arms, safeDis)
-		print(pos, "after the first star")
-		
-	#checking if new star would overlap with black hole
-	#if starsNum == 0 and pos.distance_to(blackHole.global_position) < newStar.star_radius + safeDis:
-		#var direction = (pos - blackHole.global_position).normalized()
-		#pos = blackHole.global_position + direction * (newStar.star_radius + safeDis)
-		
-	newStar.global_position = pos
+	var attempts = 0	
+	#if starsNum == 0:
+	while  attempts < MAX_ATTEMPTS:
+		# Add a random offset to the radius and angle for some randomness
+		var randomRadoffset = randf_range(-10.0,10.0)
+		var randomAngleoffset = randf_range(-0.01,0.01) 
 
-	attempts += 1
-	if attempts > MAX_ATTEMPTS:
+		# Use generate_spiral function to calculate position
+		pos = get_viewport_rect().size / 2 + generate_spiral(starsNum, Arms, safeDis + randomRadoffset)
+
+		# Check if the new position is far enough from the black hole to prevent overlap
+		if pos.distance_to(blackHole.global_position) >= newStar.star_radius + blackHole.star_radius:
+			break  # The position is valid, exit the loop
+
+		# The position is not valid, increment attempts and try again
+		attempts += 1
+
+	if attempts == MAX_ATTEMPTS:
 		print("Failed to find a valid position for the star after", MAX_ATTEMPTS, "attempts.")
-	
-#	while not is_position_valid(newStar,pos):
-#		newStar.global_position = pos
-	
+		return  # Failed to find a valid position, don't add the star
+		
 	add_child(newStar)
+	newStar.show()
+	newStar.global_position = pos
+	print(pos)
+	
+func polar_to_cartesian(r, theta):
+	#converts poar coordinates to Cartesian
+	return Vector2(r * cos(theta), r * sin(theta))
