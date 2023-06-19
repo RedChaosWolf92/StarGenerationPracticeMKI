f4a0bd72ffe8d7e23d07ca744c3e64782216d39b
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:51:30 +0100

    Update Neutron.gd

--- Galaxy/Stars/Neutron.gd
+++ Galaxy/Stars/Neutron.gd
@@ -3,13 +3,20 @@ extends StaticBody2D
 var star_size = randf_range(0.20,0.35)
 var star_radius = 0.0
 
+@export var probability: float = 0.1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	generate_Neutron()
 	
 func generate_Neutron():
-	$Neutron.scale = Vector2(star_size,star_size)
-	star_radius = star_size / 2
 	
-	self.position = Vector2(250,250)
+	var NeutronSize = $Neutron.scale
+	var NeutronRad = $NeutronRadius.shape.radius
+	var NeutronRadSize = $NeutronRadius.scale
+	
+	NeutronRad = Vector2(star_size,star_size)
+	NeutronRadSize = NeutronRad * 4
+	
+	star_radius = NeutronRadSize.length() * NeutronRad
