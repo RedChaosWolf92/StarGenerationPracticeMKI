2e06504e3d702e0aee42d607d0309a164211d2f1
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:53:02 +0100

    Update Yellow_Star.gd

--- Galaxy/Stars/Yellow_Star.gd
+++ Galaxy/Stars/Yellow_Star.gd
@@ -3,11 +3,18 @@ extends StaticBody2D
 var star_size = randf_range(0.35,.075)
 var star_radius = 0.0
 
+@export var probability: float = 0.17
+
 func _ready():
 	randomize()
 	generate_YellowStar()
 	
 func generate_YellowStar():
-	$Yellow_Star.scale = Vector2(star_size,star_size)
-	star_radius = star_size / 2
+	var YellowstarSize = $Yellow_Star.scale
+	var YellowstarRad = $YellowstarRadius.shape.radius
+	var YellowstarRadSize = $YellowstarRadius.scale
 	
+	YellowstarSize = Vector2(star_size,star_size)
+	YellowstarRadSize = YellowstarSize * 2
+	
+	star_radius = YellowstarRadSize.length() * YellowstarRad
