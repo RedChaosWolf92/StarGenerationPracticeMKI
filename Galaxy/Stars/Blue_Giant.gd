d5a47e5320dc6c014d24e457fb7e1dbbd312a0c2
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:50:22 +0100

    Update Blue_Giant.gd

--- Galaxy/Stars/Blue_Giant.gd
+++ Galaxy/Stars/Blue_Giant.gd
@@ -4,6 +4,8 @@ var star_color = Color.DEEP_SKY_BLUE
 var star_size = randf_range(0.75,1.50)
 var star_radius = 0.0
 
+@export var probability: float = 0.13
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -11,6 +13,11 @@ func _ready():
 	
 func generate_BlueGiant():
 	self.modulate = star_color
-	$Blue_Giant.scale = Vector2(star_size,star_size)
+	var BluegiantSize = $Blue_Giant.scale
+	var BluegiantRad = $BlueRadius.shape.radius
+	var BluegiantRadSize = $BlueRadius.scale
 	
-	star_radius = star_size / 2
+	BluegiantSize = Vector2(star_size,star_size)
+	BluegiantRadSize = BluegiantSize * 2
+	
+	star_radius = BluegiantRadSize.length() * BluegiantRad
