d372e5072377bb4c9491da4cdda115f28a0af29a
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:52:40 +0100

    Update White_Dwarf.gd

--- Galaxy/Stars/White_Dwarf.gd
+++ Galaxy/Stars/White_Dwarf.gd
@@ -3,13 +3,19 @@ extends StaticBody2D
 var star_size = randf_range(0.05,0.35)
 var star_radius = 0.0
 
+@export var probability: float = 0.13
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	generate_WhiteDwarf()
 	
 func generate_WhiteDwarf():
-	$White_Dwarf.scale = Vector2(star_size,star_size)
-	star_radius = star_size / 2
+	var WhitedwarfSize = $White_Dwarf.scale
+	var WhitedwarfRad = $WhitedwarfRadius.shape.radius
+	var WhitedwarfRadSize = $WhitedwarfRadius.scale
 	
-	self.position = Vector2(250,250)
+	WhitedwarfSize = Vector2(star_size,star_size)
+	WhitedwarfRadSize = WhitedwarfSize * 2
+	
+	star_radius = WhitedwarfRadSize.length() * WhitedwarfRad
