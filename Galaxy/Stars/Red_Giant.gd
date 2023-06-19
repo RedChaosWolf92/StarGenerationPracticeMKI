7bef9e20195f92ab7472954175a8f006f8b0e79d
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-19 15:52:23 +0100

    Update Red_Giant.gd

--- Galaxy/Stars/Red_Giant.gd
+++ Galaxy/Stars/Red_Giant.gd
@@ -4,6 +4,8 @@ var star_color = Color.RED.lightened(0.35)
 var star_size = randf_range(0.5,1.15)
 var star_radius = 0.0
 
+@export var probability: float = 0.13
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -11,6 +13,12 @@ func _ready():
 	
 func generate_RedGiant():
 	self.modulate = star_color
-	$Red_Giant.scale = Vector2(star_size,star_size)
+
+	var RedgiantSize = $Red_Giant.scale
+	var RedgiantRad = $RedgiantRadius.shape.radius
+	var RedgiantRadSize = $RedgiantRadius.scale
 	
-	star_radius = star_size / 2
+	RedgiantSize = Vector2(star_size,star_size)
+	RedgiantRadSize = RedgiantSize * 2
+	
+	star_radius = RedgiantRadSize.length() * RedgiantRad
