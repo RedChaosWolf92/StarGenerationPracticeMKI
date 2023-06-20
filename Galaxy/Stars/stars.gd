924929976cbbff4c7edfb03e7661ba5cde4b3e8b
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-20 13:22:35 +0100

    Update stars.gd
    this is one of two methods of generating stars, non grid method.

--- Galaxy/Stars/stars.gd
+++ Galaxy/Stars/stars.gd
@@ -3,12 +3,12 @@ extends Node2D
 var star_types = []
 const MAX_ATTEMPTS = 10
 const Spiral_Dis = 500
-const Spiral_tight = 55
+const Spiral_tight = 30
 
 var numArms = 2
 var blackHole = preload("res://Galaxy/Stars/BlackHole.tscn").instantiate() # gets the black hole node in Stars
 
-var angle_increment = PI / 250 #controlls the tightness of the spiral, higher makes spiral looser and lower makes spiral tighter
+var angle_increment = PI / 90 #controlls the tightness of the spiral, higher makes spiral looser and lower makes spiral tighter
 var current_angle = 0 # starting angle
 
 
@@ -17,23 +17,24 @@ func _ready():
 	randomize()
 	centralBlackHole()
 	#getting the child 'star' nodes
-	for child in get_children():
+	for child in get_node("StarTemplates").get_children():
 		if child is StaticBody2D:
 			star_types.append(child)
 			child.hide()
 			
 func centralBlackHole():
-	blackHole.global_position = Vector2(get_viewport_rect().size/2) # center location of the screen
-	blackHole.scale = Vector2(1.5,1.5)
-	blackHole.z_index = 1
-	add_child(blackHole)
-	blackHole.show()
-	print(blackHole.global_position, "this is where the blackhole is ")
-	print(blackHole.scale, "this is how large the black hole is")
+	var BlackHole = blackHole.duplicate()
+	BlackHole.global_position = Vector2(get_viewport_rect().size/2) # center location of the screen
+	BlackHole.scale = Vector2(5.0,5.0)
+	BlackHole.z_index = 1
+	add_child(BlackHole)
+	BlackHole.show()
+	print(BlackHole.global_position, "this is where the blackhole is ")
+	print(BlackHole.scale, "this is how large the black hole is")
 	print(" ")
 
-func generate_spiral(starsNum: int, Arms: int, safeDis: float) -> Vector2:
-	var angle = 0.34 * starsNum + Arms * (2 * PI / numArms) # adjust this value to change the distance between stars in the spiral
+func generate_spiral(starsNum: int, Arms: int, safeDis: float, current_angle: float) -> Vector2:
+	var angle = current_angle + (Arms * ((2 * PI) / numArms)) # adjust this value to change the distance between stars in the spiral
 	var rad = Spiral_Dis + Spiral_tight * angle
 	
 	#adding the safe distance tp 'rad'
@@ -44,18 +45,18 @@ func chooseStar():
 	var ran = randf()
 	var total_Prob = 0.0
 	
-	for ct in range(get_child_count()):
-		total_Prob += get_child(ct).probability
+	for star in star_types:
+		total_Prob += star.probability
 		if ran <= total_Prob:
-			return get_child(ct)
+			return star
 			
-	return get_child(0)
+	return star_types[0]
 			
 func add_star(starsNum: int):
 	var newStar = chooseStar().duplicate()
 	var Arms = starsNum % numArms
 	#finding the safe distance from blackhole
-	var safeDis = blackHole.star_radius + starsNum * 15
+	var safeDis = blackHole.star_radius + (starsNum * 15)
 	print(safeDis)
 	#position of stars
 	var pos
@@ -63,18 +64,32 @@ func add_star(starsNum: int):
 	#if starsNum == 0:
 	while  attempts < MAX_ATTEMPTS:
 		# Add a random offset to the radius and angle for some randomness
-		var randomRadoffset = randf_range(-10.0,10.0)
-		var randomAngleoffset = randf_range(-0.01,0.01) 
+		var randomRadoffset = randf_range(-360.00,360.00)
+		var randomAngleoffset = randf_range(-0.450,0.450) 
 
 		# Use generate_spiral function to calculate position
-		pos = get_viewport_rect().size / 2 + generate_spiral(starsNum, Arms, safeDis + randomRadoffset)
+		pos = get_viewport_rect().size / 2 + generate_spiral(starsNum, Arms, safeDis + randomRadoffset, current_angle + randomAngleoffset)
 
 		# Check if the new position is far enough from the black hole to prevent overlap
-		if pos.distance_to(blackHole.global_position) >= newStar.star_radius + blackHole.star_radius:
-			break  # The position is valid, exit the loop
+		if pos.distance_to(blackHole.global_position) < newStar.star_radius + blackHole.star_radius:
+			attempts += 1
+			safeDis += blackHole.star_radius
+			continue  # The position is not valid, try again
 
-		# The position is not valid, increment attempts and try again
-		attempts += 1
+		# Check if the new position is far enough from all other stars to prevent overlap
+		var overlaps = false
+		for star in get_node("StarTemplates").get_children():
+			if star != newStar and star.has_method("get_global_position") and pos.distance_to(star.get_global_position()) < newStar.star_radius + star.star_radius:
+				overlaps = true
+				safeDis = (blackHole.star_radius + (newStar.star_radius + star.star_radius)) / 2
+				break
+		if overlaps:
+			attempts += 1
+			safeDis += blackHole.star_radius
+			continue  # The position is not valid, try again
+
+		# The position is valid, exit the loop
+		break
 
 	if attempts == MAX_ATTEMPTS:
 		print("Failed to find a valid position for the star after", MAX_ATTEMPTS, "attempts.")
@@ -84,6 +99,7 @@ func add_star(starsNum: int):
 	newStar.show()
 	newStar.global_position = pos
 	print(pos)
+	current_angle += angle_increment
 	
 func polar_to_cartesian(r, theta):
 	#converts poar coordinates to Cartesian
