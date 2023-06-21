eb159dd8471b7a1c30d61ad131a834cdd5231146
RedChaosWolf92 <99774342+RedChaosWolf92@users.noreply.github.com>
2023-06-21 09:58:30 +0100

    Update Red_Giant.gd

--- Galaxy/Stars/Red_Giant.gd
+++ Galaxy/Stars/Red_Giant.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 var star_color = Color.RED.lightened(0.35)
 var star_size = randf_range(0.5,1.15)
 var star_radius = 0.0
+var safeDisfromOthers: float = 0.0
 
 @export var probability: float = 0.13
 
@@ -22,3 +23,4 @@ func generate_RedGiant():
 	RedgiantRadSize = RedgiantSize * 2
 	
 	star_radius = RedgiantRadSize.length() * RedgiantRad
+	safeDisfromOthers = star_radius
